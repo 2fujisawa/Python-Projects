e703e581c461c88a5c9d3877dd2f90b46027b15c
Linus Fujisawa <92174981+2fujisawa@users.noreply.github.com>
2024-02-26 13:43:52 -0800

    Update numberguessinggame.py

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -1,29 +1,43 @@
 import random #module random 
 
 randomnumbernum = random.randint(0,101) #generating random numbers in range of 0 to 100
+count = 0
 
-print("welcome to the number guessing game! Your objective is to guess the random number")
+print("welcome to the number guessing game! Your objective is to guess the random number with 3 tries")
 
-playernumber = int(input("Enter the number you are guessing: ")) #taking in a input as a string
+play = input("Would you like to play? ")#taking in a input as a string
+if play.lower() == 'yes': 
 
-while playernumber < 0 or playernumber > 100:
-        print("Your number is not whithin the range of 0 to 100")
-        playernumber = int(input("Reenter your guessing number: "))
+        guess = int(input("Enter your first guess: "))
+        count = count + 1
+        print("Your current count is " + str(count))
 
+        while guess < 0 or guess > 100:
+                guess = int(input("Reenter your first guess again between the range 0 and 100: "))
+                print("Your count is still " + str(count()))
+                continue
 
-while playernumber != randomnumbernum:
-    playernumber = int(input("Reenter your guessing number: "))
+        while guess != randomnumbernum:
+                if guess < randomnumbernum:
+                        playernumber = int(input("Your guessed number is lower than the random number, Reenter your guessing number: "))
+                        count = count + 1
+                        print("Your current count is " + str(count) )
 
-    if playernumber < randomnumbernum:
-                print("Your guessed number is lower than the random number")
-                continue #this is go over the loop again
+                elif guess > randomnumbernum:
+                        playernumber = int(input("Your guessed number is higher than the random number, Reenter your guessing number: "))
+                        count = count + 1
+                        print("Your current count is " + str(count))
+                        
 
-    elif playernumber > randomnumbernum:
-                print("Your guessed number is higher than the random number")
-                continue#this is go over the loop again
+                if count >=3: #if instead of elif since you want to check for the count during the while loop over an over 
+                        print("You couldn't guess the number in three tries")
+                        break
+
+        if guess == randomnumbernum:
+                print("You correctly guessed the random number")
 
 else: 
-        print("Correct, you guessed the correct number")
+        print("The game isn't going to start")
 
 
 
